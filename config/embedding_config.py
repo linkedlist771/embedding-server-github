ac2099982eb5015d300db17d59d0283f3a60afb5
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-01-30 15:22:27 +0800

    Update embedding_config.py

--- config/embedding_config.py
+++ config/embedding_config.py
@@ -2,10 +2,10 @@ import os
 
 root_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 _model_dir_path = os.path.join(root_dir, "embedding_models")
-_default_model_dir = "sentence-transformers_all-MiniLM-L6-v2"
+# _default_model_dir = "sentence-transformers_all-MiniLM-L6-v2"
 _custom_model_list = [model_dir for model_dir in os.listdir(_model_dir_path) if os.path.isdir(os.path.join(_model_dir_path, model_dir))]
 # model name should be specified in the get model function
-_custom_model_list.remove(_default_model_dir)
+# _custom_model_list.remove(_default_model_dir)
 LOGGING_DIR = "logs"
 EMBEDDING_MODEL_TYPES = ["default", "custom", ] #"ada"
 COLLECTIONS_CONFIG = [
